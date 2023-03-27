c1a661d56debfc104f8df70e47b52f1ec70bcf2a
thorne7 <darienhallam@gmail.com>
2023-03-27 21:54:51 +1030

    post request parameters

--- controllers/api/calenderRoutes.js
+++ controllers/api/calenderRoutes.js
@@ -15,7 +15,26 @@ router.get('/:id', userAuth, async (req, res) => {
   });
   const selectBed = selectbedData.get({ plain: true });
 
-    
+router.post('/:Patient', async (req, res) => {
+  try {
+    //create a new patient oject with adata from the requested body
+    const newPatientData = await new Patient({
+      name: req.body.name,
+      email:req.body.email,
+      postcode:req.body.postcode,
+      phone:req.body.phone,
+      user_id:req.body.user_id,
+      doctor:req.body.doctor_id,
+    });
+
+    req.status(200).json(newPatientData);
+  } catch (err) {
+    console.error('Failed to create patient:', err);
+    res.status(500).json({ message: 'Failed to create patient.' });
+  }
+});
+
+module.exports = router;
 // /// use differencebetween dates method (not the official name) from (vanilla or dayJS) to get the number of days
 // /// date admitted is the start date of the event of the calender
 // /// use string methods to join patient first name and last name and make it the name of the event
