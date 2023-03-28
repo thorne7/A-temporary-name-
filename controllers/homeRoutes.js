10665d7ace0540e5ee0c8d2b78cc28cd1465534d
NileshPatel83 <hselin_83@yahoo.com>
2023-03-28 19:10:28 +1000

    Date formate changed to local date format.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -98,8 +98,8 @@ async function getBedData(){
             const data = {
                 bed_id: `${bedInfo['Bed ID']}`,
                 patient_name: `${bedInfo['First Name']} ${bedInfo['Last Name']}`,
-                admit_date: `${bedInfo['Admit Date']}`,
-                discharge_Date: `${bedInfo['Dischage Date']}`,       
+                admit_date: `${bedInfo['Admit Date'].toLocaleDateString()}`,
+                discharge_Date: `${bedInfo['Dischage Date'].toLocaleDateString()}`,       
                 doctor_name: `${bedInfo.doctor['First Name']} ${bedInfo.doctor['Last Name']}`,
                 medical_condition: `${bedInfo.medical_record['Medical Condition']}`
             };
