e467fe4b0e656aa83437c14a37f07f2a5b6b1a0a
Tim <tvanmookletcher@gmail.com>
2023-03-28 22:05:12 +1030

    Basic calender implemented

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const calender = require('./calenderRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/beds', calender);
 
 module.exports = router;
