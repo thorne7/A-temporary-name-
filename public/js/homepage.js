fd493432911b15e2bb7ac247d4e3714147ccc5a7
NileshPatel83 <hselin_83@yahoo.com>
2023-03-29 21:39:30 +1000

    Route added to load calender page when bed is clicked.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,5 +1,33 @@
-const bedCard = document.querySelector('#bed-card');
-bedCard.addEventListener('click', function(event){
-event.preventDefault();
-var button = $(event.relatedTarget)
-});
+
+//Gets main container that holds all bed cards.
+const bedCardContainer = document.getElementById('bed-card-container');
+
+bedCardContainer.addEventListener('click', displayBedPage);
+
+//Gets the id of the bed that is clicked and loads beds/id page which displays patient details for ckicked bed.
+function displayBedPage(event){
+
+    event.preventDefault();
+
+    //Gets the 'bed-card' element for the bed.
+    let bedCardEl = getBedCardElement(event.target);
+    if(bedCardEl === null) return;
+    
+    //Gets the ID of the bed from data-index attribute.
+    let bedID = parseInt(bedCardEl.getAttribute('data-index')); 
+
+    document.location.replace(`/beds/${bedID}`);
+}
+
+//Gets the 'bed-card' element for the bed.
+function getBedCardElement(targetEl){
+
+    //Loops through parent elements of the element that is clicked until 'bed-card' element is found.
+    while (targetEl.className !== 'bed-card') {
+
+        targetEl = targetEl.parentElement;
+        if(targetEl === null) return null;     
+    }
+
+    return targetEl;
+}
