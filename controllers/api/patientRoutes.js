9bbc6f18bbaeffce85becf8357be64ac19cd8083
Tim <tvanmookletcher@gmail.com>
2023-04-02 20:27:31 +0930

    Updated routes, validation checks

--- controllers/api/patientRoutes.js
+++ controllers/api/patientRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const { Patient } = require('../../models');
+const { Op } = require('sequelize');
+const dayjs = require('dayjs');
+var isBetween = require('dayjs/plugin/isBetween');
+dayjs.extend(isBetween)
 
 router.post('/', async (req, res) => {
     try {
@@ -26,14 +30,14 @@ router.post('/', async (req, res) => {
     }
   });
 
-  router.get('/all', async (req, res) => {
+  router.get('/select', async (req, res) => {
     try {
         const allData = await Patient.findOne({
             order: [
                 ['id', 'DESC'],
             ],
         });
-        simpleData = allData.get({ plain: true });
+        var simpleData = allData.get({ plain: true });
         res.status(200).send(JSON.stringify(simpleData));
     } catch (err) {
         // handle any errors and send an error response
@@ -41,4 +45,48 @@ router.post('/', async (req, res) => {
         res.status(500).json({ success: false, message: 'Failed to create new patient.' });
       }
     });
+
+    router.post('/all', async (req, res) => {
+        try {
+            const allData = await validateCheck(req);
+            console.log(allData);
+            if (allData === true) {
+                res.status(200).json({ message:"true" });
+            } else {
+                res.status(400).json({ message:"false" });
+            }
+        } catch {
+            console.error(err);
+        }
+    })
+
+
+async function validateCheck(req) {
+    const today = new Date();
+    var date_admit = new Date(req.body.daVal);
+    var date_disch = new Date(req.body.ddVal);
+    if (date_disch < date_admit) {
+        return false;
+    }
+    holderArray = [];
+    const bookedbedPatients = await Patient.findAll({
+        where: {
+            bed_id: req.body.bedVal,
+        },
+    });
+    for (i=0; i < bookedbedPatients.length; i++) {
+        var startDate = new Date (bookedbedPatients[i].date_admitted);
+        var endDate = new Date (bookedbedPatients[i].date_discharge);
+        
+      if(date_admit < today || date_disch < today){
+        return false;
+      }
+      else if(date_admit > startDate && date_admit < endDate){
+        return false;
+      } else if(date_disch > startDate && date_disch < endDate){
+        return false;
+      }
+    }
+    return true;
+}
   module.exports = router;
